c6493cd02fd8920746299f784bdef8a4dcb4b75e
DimasRachmanHkm <rachman242003@gmail.com>
2023-03-03 15:15:53 +0700

    commit v1.2

--- latihan.py
+++ latihan.py
@@ -1,8 +1,15 @@
-for i in range(1,101):
-    print(i)
+#for i in range(1,101):
+    #print(i)
         
-for i in range(1,101):
+'''for i in range(1,101):
     if(i%2 == 0):
      continue
     else:
-       print(i)
+       print(i)'''
+
+def is_ood(angka):
+    return angka % 2 == 1
+
+for i in range (1,101):
+    if is_ood(i):
+        print(i)
